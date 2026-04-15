4252cc28db2dda9bb836e7ba24a7bbcf46cfa812
Davicyber <0001150848@senaimgaluno.com.br>
2026-04-15 14:18:26 -0300

    Add files via upload

---
+++ codbasico.py
@@ -0,0 +1,9 @@
+nome = input("Digite o seu nome: ")
+print(f"Olá, {nome}! Vamos fazer uma soma.")
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(nput("Digite o segundo número: "))
+
+soma = num1 + num2
+
+print(f"A soma de {num1} + {num2} é: {soma}")
